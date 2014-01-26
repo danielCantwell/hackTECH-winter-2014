08dbcedcc6d1f81555e89e2336061277fa027733
kuoloneous <albert.kuo.123@gmail.com>
2014-01-25 18:37:50 -0800

    Changed project and added images

---
+++ SPDrinksProject/SPDrinks/src/main/java/com/spdrinks/Drink.java
@@ -0,0 +1,57 @@
+package com.spdrinks;
+
+/**
+ * Created by Albert on 1/25/14.
+ *
+ * TABLE INFORMATION
+ *
+ * ID           |INT        |PRI
+ * session_id   |INT       |
+ * time_created |TEXT       |
+ */
+public class Drink {
+
+    int _id;
+    int _session_id;
+    String _time_created;
+
+    public Drink(){
+
+    }
+
+    public Drink(int id, int session_id, String time_created){
+        this._id = id;
+        this._session_id = session_id;
+        this._time_created = time_created;
+
+    }
+
+    public Drink(int session_id, String time_created){
+        this._session_id = session_id;
+        this._time_created = time_created;
+    }
+
+    public int getID(){
+        return this._id;
+    }
+
+    public void setID(int id){
+        this._id = id;
+    }
+    public int getSessionID(){
+        return this._session_id;
+    }
+
+    public void setSessionID(int session_id){
+        this._session_id = session_id;
+    }
+
+    public String getTimeCreated(){
+        return this._time_created;
+    }
+
+    public void setTimeCreated(String time){
+        this._time_created = time;
+    }
+
+}
---
+++ SPDrinksProject/SPDrinks/src/main/java/com/spdrinks/DrinkSession.java
@@ -0,0 +1,7 @@
+package com.spdrinks;
+
+/**
+ * Created by Albert on 1/25/14.
+ */
+public class DrinkSession {
+}
